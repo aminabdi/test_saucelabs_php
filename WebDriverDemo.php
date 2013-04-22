75e59b997846565a21a8c2df56a1acb8ff475d58
Parawuit Pothisaka <whiterabbit13@gmail.com>
2013-04-22 16:09:59 +0700

    update

--- WebDriverDemo.php
+++ WebDriverDemo.php
@@ -34,41 +34,6 @@ class WebDriverDemo extends Sauce\Sausage\WebDriverTestCase
         $this->setBrowserUrl('http://saucelabs.com/test/guinea-pig');
     }
 
-    public function testTitle()
-    {
-        $this->assertContains("I am a page title", $this->title());
-    }
 
-    public function testLink()
-    {
-        $link = $this->byId('i am a link');
-        $link->click();
-        $this->assertContains("I am another page title", $this->title());
-    }
-
-    public function testTextbox()
-    {
-        $test_text = "This is some text";
-        $textbox = $this->byId('i_am_a_textbox');
-        $textbox->click();
-        $this->keys($test_text);
-        $this->assertEquals($textbox->value(), $test_text);
-    }
-
-    public function testSubmitComments()
-    {
-        $comment = "This is a very insightful comment.";
-        $this->byId('comments')->value($comment);
-        $this->byId('submit')->submit();
-        $driver = $this;
-
-        $comment_test = function() use ($comment, $driver) {
-            $text = $driver->byId('your_comments')->text();
-            return $text == "Your comments: $comment";
-        };
-
-        $this->spinAssert("Comment never showed up!", $comment_test);
-
-    }
 
 }
